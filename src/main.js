ff02e7bab10df59c7622ebe921d4ac8471a7d0d3
Tawnee Harris <tawneeh@icloud.com>
2020-11-30 20:27:00 -0800

    add dev goal notes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css'; // might be incorrect
+import './css/styles.css';
 
 // main.js is for UI logic
+
+// allow users to add tasks to the list (append list items)
+// allow users to indicate that a task is done
+// allow users to remove a list item (how??)
+// write specs in ReadMe.md and run Jest tests?
